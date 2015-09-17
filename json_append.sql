772ec718119245b7b7427d299fa7e657daf1ef9e
eddiechong <eddie.chong@cbsinteractive.com>
2015-09-17 18:01:25 +1000

    Create json_append.sql
    Function to append json to another json

---
+++ json_append.sql
@@ -0,0 +1,18 @@
+-- Function: json_append(json, json)
+
+-- DROP FUNCTION json_append(json, json);
+
+CREATE OR REPLACE FUNCTION json_append(data json, insert_data json)
+  RETURNS json AS
+$BODY$
+    SELECT ('{'||string_agg(to_json(key)||':'||value, ',')||'}')::json
+    FROM (
+        SELECT * FROM json_each(data)
+        UNION ALL
+        SELECT * FROM json_each(insert_data)
+    ) t;
+$BODY$
+  LANGUAGE sql IMMUTABLE
+  COST 100;
+ALTER FUNCTION json_append(json, json)
+  OWNER TO postgres;
